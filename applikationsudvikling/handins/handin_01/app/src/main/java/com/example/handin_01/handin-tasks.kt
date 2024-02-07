e9d8768663d852756898a42bb7fda6771cbb59da
Emythiel <emythiel@pm.com>
2024-02-07 14:14:31 +0100

    Initial handin 1 tasks kotlin file

---
+++ applikationsudvikling/handins/handin_01/app/src/main/java/com/example/handin_01/handin-tasks.kt
@@ -0,0 +1,79 @@
+package com.example.handin_01
+
+// Run the different handin tasks
+fun main() {
+
+}
+
+/*
+    1.
+    A person is eligible to voite if his/her age is greater than or equal to 18.
+    Define a method to find out if he/she is eligible to vote.
+    Let the user input their age.
+*/
+
+
+/*
+    2.
+    Define two functions to print the maximum and the minimum number respectively,
+    among three numbers.
+*/
+
+
+/*
+    3.
+    Write a Kotlin function named 'calculateAverage'
+    that takes in a list of numbers and returns their average
+*/
+
+
+/*
+    4.
+    Write a number that returns if a user has input a valid CPR number.
+    A valid CPR number has:
+        - 10 Digits.
+        - First 2 digits are not above 31
+        - Middle 2 digits are not above 12
+    The method returns true if the CPR number is valid, false if it is not.
+*/
+
+
+/*
+    5.
+    Write a program that prints the numbers from 1 to 100.
+    But for multiples of three, print "Fizz" instead of the number,
+    and for multiples of five, print "Buzz".
+    For numbers which are multiples of both three and five, print "FizzBuzz".
+*/
+
+
+/*
+    6.
+    Write a program that takes your full name as input and displays the
+    abbreviations of the first and middle names except the last name which is
+    displayed as it is.
+    Eg. "Robert Brett Roser" becomes "R.B. Roser".
+*/
+
+
+/*
+    7.
+    Write a program that takes a numerical grade (0-100) as input and prints the
+    corresponding American letter grade.
+    Implement a function, 'calculateGrade', that takes an integer parameter
+    representing the grade and returns a string representing the letter representing
+    the letter grade according to the following scale:
+        - 90-100: "A"
+        - 80-89: "B"
+        - 70-79: "C"
+        - 60-69: "D"
+        - Below 60: "F"
+*/
+
+
+/*
+    8. Write a Kotlin function named filterWordsByLength that takes in a list of strings,
+    and returns a a list containing only the words that have a length greater than
+    or equal to the specified minimum length.
+        - Use filter function and Lambda expressions
+*/
