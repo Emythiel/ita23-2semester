5c7125c57428848b980531049a98677f3680199e
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-02-14 12:03:52 +0100

    Prepped handin 2 tasks

--- applikationsudvikling/handins/app/src/main/java/com/example/handins/handin02/handin02-tasks-emily.kt
+++ applikationsudvikling/handins/app/src/main/java/com/example/handins/handin02/handin02-tasks-emily.kt
@@ -1,2 +1,83 @@
 package com.example.handins.handin02
 
+fun main() {
+    // Task 1 -- Employee Salary
+    //h1employeeSalaries()
+
+    // Task 2 -- Computer, Laptop, SmartPhone
+    //h2computer()
+
+    // Task 3 -- Product Category Identification
+    //h3productCategory()
+
+    // Task 4 -- Shapes and Perimeter/Area Calculation
+    //h4shapes()
+}
+
+
+/*
+    1)
+    Create a class called employee that includes three peices
+    of information as instance variables.
+        - A first name
+        - A last name
+        - A monthly salary
+    Class should have a constructor that inits the three variables.
+    If the monthly salary is not positive, set it to 0.0.
+    Create two employee objects and display each object's yearly salary.
+    Then give each employee a 10% raise and display each
+    employee's yearly salary again.
+*/
+fun h1employeeSalaries() {
+
+}
+
+/*
+    2)
+    Create a new class called Computer.
+    Two additional classes will be needed: Laptop and SmartPhone.
+        1. For a parent class, add 3 properties, 2 methods and
+           a constructor.
+        2. For a child class, add at least 1 additional property
+           and 1 additional method.
+    In the main method, create a Laptop and SmartPhone.
+*/
+fun h2computer() {
+
+}
+
+/*
+    3)
+    Create a base class Product with attributes like name, price, and
+    quantity, as well as a function identifyProductCategory.
+    Subclass it to create specific product types like Shoe,
+    T-shirt, and Book. Override the function such taht:
+        - The shoe outputs "I am a shoe".
+        - The T-shirt outputs "I am a T-shirt".
+        - The book outputs "I am a book".
+*/
+fun h3productCategory() {
+
+}
+
+/*
+    4)
+    Create 3 classes, Circle, Rectangle, and Triangle, with parent
+    class Shape.
+        - Every shape has two attributes:
+            - color
+            - isTransparent
+        - Every class has private attributes to calculate perimeter
+          and area for each shape.
+            - E.g. Rectangle has attributes: height, width, color &
+              isTransparent.
+                - The attributes are set in the constructor
+        - The 3 classes overrides the following abstract
+          methods from their parent class:
+            - calculatePerimeter
+            - calculateArea
+        - The functions will return the perimeter or area of the shape.
+*/
+fun h4shapes() {
+
+}
